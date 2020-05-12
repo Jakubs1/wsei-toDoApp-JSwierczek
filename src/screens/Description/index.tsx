731431416b8eaa83566d64a0718ea7ea73c64d29
Jakubs1 <jakub.swierczek@microsoft.wsei.edu.pl>
2020-05-13 00:42:58 +0200

    added some css to 1, 3 screens

--- src/screens/Description/index.tsx
+++ src/screens/Description/index.tsx
@@ -3,10 +3,44 @@ import { Text, View, Image, ScrollView } from 'react-native';
 import styled from 'styled-components/native';
 import { FC } from 'react';
 
+
+const GuitarImg = styled.Image`
+width: 170px;
+height: 100px;
+margin: 10% 0 0 5%;
+`
+const RockImg = styled.Image`
+width: 200px;
+height: 100px;
+margin-left: 40%;
+`
+
+const Lorem = styled.Text`
+    margin: 20px 20px 10px 20px;
+    font-size: 13px;
+`;
+
+
 const DescriptionTab: FC = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>There will be description</Text>
+        <View>
+            <GuitarImg source={require('../../assets/guitar.jpg')}
+
+            />
+            <Lorem>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et mattis ipsum, et efficitur orci. Etiam
+            consequat ornare dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos. Nullam commodo, elit viverra iaculis convallis, massa quam viverra odio, eu facilisis orci
+            sem id mi. Cras scelerisque euismod orci, non mattis arcu rutrum in. Fusce eu tortor vitae neque
+            molestie dapibus id ut elit. Pellentesque tellus nisl, maximus eget purus tristique, vehicula ultrices
+                nibh.</Lorem>
+            <RockImg source={require('../../assets/rock.jpg')}
+
+            />
+
+            <Lorem>Vestibulum at feugiat mauris, nec ornare enim. Etiam nunc lectus, elementum eu justo id, ultricies
+            tempor turpis. Etiam eleifend mauris sed mauris feugiat dapibus. Proin fringilla vulputate lorem, ac
+            congue ipsum dictum a. Pellentesque velit arcu, faucibus eget quam et, ullamcorper sagittis est. Quisque
+            ac felis iaculis, pellentesque sem et, lacinia ante. Maecenas in urna lorem. Nulla facilisi.</Lorem>
         </View>
     );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, Image, ScrollView } from 'react-native';
+import { Text } from 'react-native';
 import styled from 'styled-components/native';
 import { FC } from 'react';
 
@@ -7,21 +7,30 @@ const Avatar = styled.Image`
     height: 100px;
     width: 100px;
     border-radius: 50px;
-    margin-top: 30px;
+    margin-top: 40px;
     margin-left: 15px;
+    margin-bottom: 10px;
 `;
 const LandscapeImg = styled.Image`
-    width: 100%;
-    height: 40%;
+    width: 320px;
+    height: 200px;
+    margin: 10px auto;
 `;
 export const ScrollBox = styled.ScrollView`
     flex: 1;
     width: 100%;
 `;
+
+const Welcome = styled.Text`
+    font-size: 20px;
+    margin: 0 auto;
+    font-weight: bold;
+`
+
 const Paragraph = styled.Text`
     margin: 20px;
-    font-size: 18px;
-    line-height: 28px;
+    font-size: 12px;
+    line-height: 24px;
 `;
 
 const HomeTab: FC = () => {
@@ -31,11 +40,11 @@ const HomeTab: FC = () => {
             <Avatar
                 source={require('../../assets/avatar.png')}
             />
-            <Text>There will be Home Page</Text>
+            <Welcome>Welcome</Welcome>
             <LandscapeImg
                 source={require('../../assets/wolf.jpg')}
             />
-            <Paragraph>{'\t'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paragraph>
+            <Paragraph>{'\t'}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a commodi accusamus. Sapiente numquam exercitationem optio doloribus laboriosam, dicta nihil et quae tempore blanditiis nisi non amet iste atque similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati molestias modi eos fugit. Vero voluptate labore, accusamus facere dolores quas distinctio animi sapiente tempore eligendi eum voluptas beatae doloribus aperiam?</Paragraph>
         </ScrollBox>
     );
 }
